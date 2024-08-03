8de158fb65dc3db1724e32c1d7a6835410a4b6e9
vbarda <vadim.barda@gmail.com>
2024-08-03 11:24:38 -0400

    dotenv

--- app/llama_dwight/llms.py
+++ app/llama_dwight/llms.py
@@ -17,9 +17,9 @@ OLLAMA_MODELS = frozenset({LLMName.OLLAMA_3_1_8B})
 GROQ_MODELS = frozenset({LLMName.GROQ_LLAMA_3_1_8B, LLMName.GROQ_LLAMA_3_1_70B})
 
 
-def get_llm(name: LLMName) -> BaseChatModel:
+def get_llm(name: LLMName, local: bool = False) -> BaseChatModel:
     if name in OLLAMA_MODELS:
-        host_ip = os.environ.get("HOST_IP", "not set")
+        host_ip = "localhost" if local else os.environ.get("HOST_IP", "not set")
         ollama_url = f"http://{host_ip}:11434"
         return ChatOllama(model=name, temperature=0, base_url=ollama_url)
     elif name in GROQ_MODELS:
