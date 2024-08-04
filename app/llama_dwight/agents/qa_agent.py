e537f8413951c1af926a27779190b21c161a28f3
vbarda <vadim.barda@gmail.com>
2024-08-03 22:04:25 -0400

    prompt improvements

--- app/llama_dwight/agents/qa_agent.py
+++ app/llama_dwight/agents/qa_agent.py
@@ -6,12 +6,13 @@ from llama_dwight.tools.base import BaseDataToolKit
 
 SYSTEM_PROMPT = """You are an experienced data analyst that has access to a dataset with the following schema: {schema}."
 When answering complex questions, think step by step. Break the problem down into a series of the following steps:
-- first, apply filters (if any)
-- then, apply sort + limit (if any)
-- finally, apply aggregations
+- first, use `filter` (if relevant)
+- then, use `sort` (if any)
+- finally, use `aggregate` or `groupby`. Never apply `aggregate` after `groupby`.
 
-Always use a single tool at a time.
-Only use column names from the originally provided schema.
+REMEMBER:
+- Only use column names from the originally provided schema.
+- Only respond with the answer to the original question, do not mention the tools you used.
 
 Now start with the first tool you need to call. Begin!"""
 
